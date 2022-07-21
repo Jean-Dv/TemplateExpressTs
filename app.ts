d24386cd22d381ae93dac779779e467fc1aa1472
Jean-Dv <mrjunior127@gmail.com>
2022-07-20 23:20:04 -0500

    Added app.ts for use class server

---
+++ app.ts
@@ -0,0 +1,11 @@
+import { Server } from "./bin/server"
+import dotenv from 'dotenv'
+
+dotenv.config()
+const server = new Server()
+const appServer = server.app
+const logger = server.logger
+const routePrefix = server.routePrefix
+server.start()
+
+export { server, appServer, routePrefix, logger}
