48fe3c02dbd2f26870a2741f1b125b28e950f015
Jean-Dv <mrjunior127@gmail.com>
2022-07-20 23:30:36 -0500

    test for ping pong endpoint

---
+++ __test__/home.spec.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+import { appServer, server, routePrefix } from '../app'
+
+describe(`GET ${routePrefix}/ping`, () => {
+  test('should return 200 and text pong', async () => {
+    const response = await request(appServer)
+      .get(`${routePrefix}/ping`)
+    expect(response.statusCode).toBe(200)
+    expect(response.body.message).toMatch('Pong')
+  })
+})
+
+afterAll(() => {
+  server.close()
+})
