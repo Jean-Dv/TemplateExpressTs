200db79f7ad4bc0a00e1c58f6002d8de20b7d7bb
Jean-Dv <mrjunior127@gmail.com>
2022-09-13 11:53:51 -0500

    Fix: error with not import config in json

--- configs/logger/index.ts
+++ configs/logger/index.ts
@@ -1,6 +1,7 @@
 import log4js from 'log4js'
+import log4json from '@configs/logger/log4js.json'
 
-log4js.configure('./configs/logger/log4js.json')
+log4js.configure(log4json)
 
 const logger = {
   access: log4js.getLogger('access'),
